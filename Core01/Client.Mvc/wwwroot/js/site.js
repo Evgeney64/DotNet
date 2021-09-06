2f5774668463d83b29be4f2c1d5aecce4c15f3e9
Evgeney64 <86039069+Evgeney64@users.noreply.github.com>
2021-09-06 21:21:45 +0300

    20210712-01

--- Core01/Client.Mvc/wwwroot/js/site.js
+++ Core01/Client.Mvc/wwwroot/js/site.js
@@ -2,3 +2,52 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function Site_Border() {
+
+    var sel = $("#body_id");
+    if (sel != undefined)
+        sel.css("border", "3px solid blue");
+
+    Ext.onReady(function () {
+        Ext.create('Ext.Panel', {
+            width: 500,
+            height: 360,
+            padding: 10,
+            layout: 'border',
+            items: [{
+                xtype: 'panel',
+                title: 'Центральная панель',
+                html: 'Центральная панель',
+                region: 'center',
+                margin: '5 5 5 5'
+            }, {
+                xtype: 'panel',
+                title: 'Верхняя панель',
+                html: 'Верхняя панель',
+                region: 'north',
+                height: 80
+            }, {
+                xtype: 'panel',
+                title: 'Нижняя панель',
+                html: 'Нижняя панель',
+                region: 'south',
+                height: 80
+            }, {
+                xtype: 'panel',
+                title: 'Левая панель',
+                html: 'Левая панель',
+                region: 'west',
+                width: 100
+            }, {
+                xtype: 'panel',
+                title: 'Правая панель',
+                html: 'Правая панель',
+                region: 'east',
+                width: 120
+            }],
+            renderTo: Ext.getBody()
+        });
+    });
+
+}
+
