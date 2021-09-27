a49d8053955e3b074b717dc949122758c196ec13
Евгений Роев <roev64@mail.ru>
2021-09-27 12:17:32 +0500

    20210712-01

---
+++ metanit/03.Dependency Injection/Client.Mvc/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace Home.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
