492b9b2c54eca18b5f5a761837e3c6b79b71340b
Евгений Роев <roev64@mail.ru>
2021-10-01 13:28:39 +0500

    20210712-01

--- metanit/21.Аутентификация и авторизация/Client.Mvc/Controllers/AccountController.cs
+++ metanit/21.Аутентификация и авторизация/Client.Mvc/Controllers/AccountController.cs
@@ -8,10 +8,20 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.Extensions.Configuration;
 using System.Linq;
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+//using TokenApp.Models; // класс Person
+
 using Server.Core.Public;
 using Server.Core.ViewModel;
 using Server.Core.CoreModel;
 using Server.Core.AuthModel;
+using ru.tsb.mvc;
 
 namespace AuthApp.Controllers
 {
@@ -110,6 +120,61 @@ namespace AuthApp.Controllers
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             return RedirectToAction("Login", "Account");
+        }
+
+        [HttpPost("/token")]
+        public IActionResult Token(string username, string password)
+        {
+            var identity = GetIdentity(username, password);
+            if (identity == null)
+            {
+                return BadRequest(new { errorText = "Invalid username or password." });
+            }
+
+            var now = DateTime.UtcNow;
+            // создаем JWT-токен
+            var jwt = new JwtSecurityToken(
+                    issuer: AuthOptions.ISSUER,
+                    audience: AuthOptions.AUDIENCE,
+                    notBefore: now,
+                    claims: identity.Claims,
+                    expires: now.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
+                    signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            var response = new
+            {
+                access_token = encodedJwt,
+                username = identity.Name
+            };
+
+            return Json(response);
+        }
+
+        private ClaimsIdentity GetIdentity(string username, string password)
+        {
+            VmBase vmBase = new VmBase(configuration, ConnectionType_Enum.Auth);
+
+            scr_user user = vmBase.Users
+                .Where(ss => ss.email == username)
+                .FirstOrDefault();
+
+            //Person person = people.FirstOrDefault(x => x.Login == username && x.Password == password);
+            if (user != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimsIdentity.DefaultNameClaimType, user.email),
+                    new Claim(ClaimsIdentity.DefaultRoleClaimType, user.role)
+                };
+                ClaimsIdentity claimsIdentity =
+                new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
+                    ClaimsIdentity.DefaultRoleClaimType);
+                return claimsIdentity;
+            }
+
+            // если пользователя не найдено
+            return null;
         }
     }
 }
--- metanit/21.Аутентификация и авторизация/Client.Mvc/Startup.cs
+++ metanit/21.Аутентификация и авторизация/Client.Mvc/Startup.cs
@@ -80,7 +80,7 @@ namespace ru.tsb.mvc
 
             #region 21.Аутентификация и авторизация
             #region 01 - Аутентификация на основе куки
-            if (1 == 1)
+            if (1 == 2)
             {
                 // установка конфигурации подключения
                 services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
@@ -93,7 +93,7 @@ namespace ru.tsb.mvc
             #endregion
 
             #region 02 - Авторизация на основе Claims
-            if (1 == 1)
+            if (1 == 2)
             {
                 services.AddAuthorization(opts => {
                     //opts.AddPolicy("OnlyForLondon", policy => {
@@ -107,7 +107,7 @@ namespace ru.tsb.mvc
             #endregion
 
             #region 03 - Создание ограничений для политики авторизации
-            if (1 == 1)
+            if (1 == 2)
             {
                 services.AddTransient<IAuthorizationHandler, AgeHandler>();
 
@@ -120,7 +120,7 @@ namespace ru.tsb.mvc
             #endregion
 
             #region 04 - Авторизация с помощью JWT-токенов
-            if (1 == 2)
+            if (1 == 1)
             {
                 services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                     .AddJwtBearer(options =>
