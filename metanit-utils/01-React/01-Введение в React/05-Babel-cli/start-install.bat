bc75810298a3050ad696d63248be72932e51493e
Евгений Роев <roev64@mail.ru>
2021-10-15 09:36:24 +0500

    20211010-01

---
+++ metanit-utils/01-React/01-Введение в React/05-Babel-cli/start-install.bat
@@ -0,0 +1,3 @@
+c:\Disc_C\_Setup\Java\node-v16.4.2\npm install
+pause
+
---
+++ metanit-utils/01-React/01-Введение в React/05-Babel-cli/start.bat
@@ -0,0 +1,9 @@
+echo http://localhost:3000/
+
+c:\Disc_C\_Setup\Java\node-v16.4.2\npm install
+
+c:\Disc_C\_Setup\Java\node-v16.4.2\npx babel index.jsx --out-file index.js
+
+c:\Disc_C\_Setup\Java\node-v16.4.2\node app.js
+pause
+
