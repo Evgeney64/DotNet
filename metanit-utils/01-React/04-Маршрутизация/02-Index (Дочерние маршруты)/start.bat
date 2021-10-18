dc8c752bb8bee0de431d18edeb9765fd7407feaa
Евгений Роев <roev64@mail.ru>
2021-10-18 16:57:23 +0500

    20211010-01

---
+++ metanit-utils/01-React/04-Маршрутизация/02-Index (Дочерние маршруты)/start.bat
@@ -0,0 +1,11 @@
+echo http://localhost:3000/
+echo http://localhost:3000/about
+echo http://localhost:3000/products
+echo http://localhost:3000/products/phones
+echo http://localhost:3000/products/tablets
+echo http://localhost:3000/ddd
+
+set PATH=%PATH%;c:\DISC_C\_Setup\Java\node-v14.18.1
+node app
+pause
+
