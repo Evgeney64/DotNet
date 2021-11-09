b3ef2ad33cfa4ebad002be32b3ee6f3de705a806
Евгений Роев <roev64@mail.ru>
2021-11-09 12:43:50 +0500

    20211010-01

--- Extentions/EdmGen/Generate/Generate.cs
+++ Extentions/EdmGen/Generate/Generate.cs
@@ -73,137 +73,77 @@ namespace Tsb.Generate
             if (tbl == null)
                 return new ServiceResult("Недопустимая таблица", true);
 
-            CodeCompileUnit servOneUnit = null;
+            CodeCompileUnit servOneUnit = new CodeCompileUnit();
             CodeNamespace servOneNamespace = null;
             CodeNamespace servOneModelNamespace = null;
-            bool is_servFirst = true;
-            bool oneFile_Serv = true;
-            if (oneFile_Serv)
+
+            TsbCodeGenResult item_class = new TsbCodeGenResult
             {
-                servOneUnit = new CodeCompileUnit();
-                TsbCodeGenResult item_class = new TsbCodeGenResult
+                Class_Serv = new CodeTypeDeclaration
                 {
-                    Class_Serv = new CodeTypeDeclaration
-                    {
-                        Name = name,
-                        IsPartial = true,
-                    },
-                    Namespace_Serv = new CodeNamespace("Server.Core.Model"),
+                    Name = name,
+                    IsPartial = true,
+                },
+                Namespace_Serv = new CodeNamespace("Server.Core.Model"),
+            };
+
+            #region columns
+            foreach (column col in tbl.columns)
+            {
+                col.typeClrSet();
+                CodeMemberField prop = new CodeMemberField
+                {
+                    Name = col.name + " { get; set; }",
+                    Attributes = MemberAttributes.Public | MemberAttributes.Final,
+                    Type = new CodeTypeReference(col.typeClr),
                 };
-                foreach(column col in tbl.columns)
+                if (col.is_primary_key == 1)
                 {
-                    col.typeClrSet();
-                    CodeMemberField prop = new CodeMemberField
-                    {
-                        Name = col.name + " { get; set; }",
-                        Attributes = MemberAttributes.Public | MemberAttributes.Final,
-                        Type = new CodeTypeReference(col.typeClr),
-                    };
-                    if (col.is_primary_key == 1)
-                    {
-                        prop.CustomAttributes.Add(new CodeAttributeDeclaration("KeyAttribute"));
-                        //prop.CustomAttributes.Add(new CodeAttributeDeclaration(
-                        //    "System.ComponentModel.DataAnnotations.KeyAttribute",
-                        //    new CodeAttributeArgument(new CodePrimitiveExpression(""))));
-                    }
-                    item_class.Class_Serv.Members.Add(prop);
+                    prop.CustomAttributes.Add(new CodeAttributeDeclaration("KeyAttribute"));
+                    //prop.CustomAttributes.Add(new CodeAttributeDeclaration(
+                    //    "System.ComponentModel.DataAnnotations.KeyAttribute",
+                    //    new CodeAttributeArgument(new CodePrimitiveExpression(""))));
                 }
-                //List<TsbCodeGenResult> res = new List<TsbCodeGenResult>();
-                //res.Add(item);
-                //foreach (var res_item in res)
-                {
-                    #region
-                    #region interfaces
-                    item_class.Class_Serv.BaseTypes.Add("IEntityObject");
-                    item_class.Class_Serv.BaseTypes.Add("IEntityLog");
-                    item_class.Class_Serv.BaseTypes.Add("IEntityPeriod");
-                    #endregion
-
-                    if (!item_class.Is_Error)
-                    {
-                        if ((oneFile_Serv) && (item_class.Class_Serv != null))
-                        {
-                            if (is_servFirst)
-                            {
-                                //servOneUnit.Namespaces.Add(res_item.Namespace_Serv_Using);
-                                servOneNamespace = item_class.Namespace_Serv;
-                                // !!!
-                                servOneNamespace.Types.Add(item_class.Class_Serv);
-                                //
-                                is_servFirst = false;
-                            }
-
-
-                            // !!!
-                            ////servOneNamespace.Types.Add(res_item.Class_Serv);
-                            //if (item_class.Class_Serv_Entity != null)
-                            //    servOneNamespace.Types.Add(item_class.Class_Serv_Entity);
-                            //if (item_class.Class_Serv_IContextWithGroup != null)
-                            //    servOneNamespace.Types.Add(item_class.Class_Serv_IContextWithGroup);
-
-                            //if (item_class.Class_Serv_Metadata != null)
-                            //{
-                            //    if (servOneModelNamespace == null)
-                            //    {
-                            //        servOneModelNamespace = item_class.Namespace_Model;
-                            //    }
-                            //    servOneModelNamespace.Types.Add(item_class.Class_Serv_Metadata);
-                            //}
-
-                            //if (item_class.Class_Serv_Tree != null)
-                            //{
-                            //    if (servOneModelNamespace == null)
-                            //    {
-                            //        servOneModelNamespace = item_class.Namespace_Model;
-                            //    }
-                            //    servOneModelNamespace.Types.Add(item_class.Class_Serv_Tree);
-                            //}
-
-                            //if (item_class.Class_Serv_Edm != null)
-                            //{
-                            //    if (servOneModelNamespace == null)
-                            //    {
-                            //        servOneModelNamespace = item_class.Namespace_Model;
-                            //    }
-                            //    servOneModelNamespace.Types.Add(item_class.Class_Serv_Edm);
-                            //}
-
-                            item_class.FilePath_Serv = dir + "//" + name + ".cs";
-                        }
-                    }
-                    #endregion
-                }
-
-                if (servOneNamespace != null)
-                {
-                    servOneNamespace.Imports.Add(new CodeNamespaceImport("System"));
-                    servOneNamespace.Imports.Add(new CodeNamespaceImport("System.Collections.Generic"));
-                    servOneNamespace.Imports.Add(new CodeNamespaceImport("System.ComponentModel.DataAnnotations"));
-                    servOneNamespace.Imports.Add(new CodeNamespaceImport("System.ComponentModel.DataAnnotations.Schema"));
-                    servOneNamespace.Imports.Add(new CodeNamespaceImport("Server.Core.Public"));
-
-                    servOneUnit.Namespaces.Add(servOneNamespace);
-                    //CodeTypeReference typeRef1 = new CodeTypeReference("System.DateTime");
-
-                    if (servOneModelNamespace != null)
-                    {
-                        servOneUnit.Namespaces.Add(servOneModelNamespace);
-                    }
-
-                    #region save Edm class
-                    string codeFileName_Serv = dir + "//" + name + ".cs";
-                    using (var outFile = File.Open(codeFileName_Serv, FileMode.Create))
-                    using (var fileWriter = new StreamWriter(outFile))
-                    using (var indentedTextWriter = new IndentedTextWriter(fileWriter, "    "))
-                    {
-                        var provider = new Microsoft.CSharp.CSharpCodeProvider();
-                        provider.GenerateCodeFromCompileUnit(servOneUnit,
-                            indentedTextWriter,
-                            new CodeGeneratorOptions() { BracingStyle = "C" });
-                    }
-                    #endregion
-                }
+                item_class.Class_Serv.Members.Add(prop);
             }
+            #endregion
+
+            #region interfaces
+            item_class.Class_Serv.BaseTypes.Add("IEntityObject");
+            item_class.Class_Serv.BaseTypes.Add("IEntityLog");
+            item_class.Class_Serv.BaseTypes.Add("IEntityPeriod");
+            #endregion
+
+            servOneNamespace = item_class.Namespace_Serv;
+            servOneNamespace.Types.Add(item_class.Class_Serv);
+            item_class.FilePath_Serv = dir + "//" + name + ".cs";
+
+            #region uses
+            servOneNamespace.Imports.Add(new CodeNamespaceImport("System"));
+            servOneNamespace.Imports.Add(new CodeNamespaceImport("System.Collections.Generic"));
+            servOneNamespace.Imports.Add(new CodeNamespaceImport("System.ComponentModel.DataAnnotations"));
+            servOneNamespace.Imports.Add(new CodeNamespaceImport("System.ComponentModel.DataAnnotations.Schema"));
+            //servOneNamespace.Imports.Add(new CodeNamespaceImport("Server.Core.Public"));
+            servOneUnit.Namespaces.Add(servOneNamespace);
+            #endregion
+
+            if (servOneModelNamespace != null)
+            {
+                servOneUnit.Namespaces.Add(servOneModelNamespace);
+            }
+
+            #region save Edm class
+            string codeFileName_Serv = dir + "//" + name + ".cs";
+            using (var outFile = File.Open(codeFileName_Serv, FileMode.Create))
+            using (var fileWriter = new StreamWriter(outFile))
+            using (var indentedTextWriter = new IndentedTextWriter(fileWriter, "    "))
+            {
+                var provider = new Microsoft.CSharp.CSharpCodeProvider();
+                provider.GenerateCodeFromCompileUnit(servOneUnit,
+                    indentedTextWriter,
+                    new CodeGeneratorOptions() { BracingStyle = "C" });
+            }
+            #endregion
 
             return new ServiceResult("Файл сохранен");
             #endregion
