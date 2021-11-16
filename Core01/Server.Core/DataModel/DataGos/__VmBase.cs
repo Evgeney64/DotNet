dcd94a68d7ea873668bea5a490056a88eca32035
Евгений Роев <roev64@mail.ru>
2021-11-16 13:16:37 +0500

    20211010-01

--- Core01/Server.Core/DataModel/DataGos/__VmBase.cs
+++ Core01/Server.Core/DataModel/DataGos/__VmBase.cs
@@ -33,7 +33,7 @@ namespace Server.Core
             {
                 List<Partners> pars = _serv.Get_Partners().Where(ss => ss.par_id <= 10).ToList();
                 List<payerlive> plvs = _serv.Get_payerlive().Where(ss => ss.reciever_id <= 10).ToList();
-                //List<rgn> rgns = _serv.Get_rgn().Where(ss => ss.rgn_id <= 10).ToList();
+                List<rgn> rgns = _serv.Get_rgn().Where(ss => ss.rgn_id <= 10).ToList();
             }
         }
         #endregion
