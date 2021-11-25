e524697d49ab987078e016e511e44eaf07ba0ec1
Евгений Роев <roev64@mail.ru>
2021-11-25 16:43:22 +0500

    20211010-01

---
+++ Core01/Server.Core/DataModel/DataGos/VmBase/VmBase1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.Extensions.Configuration;
+
+using Server.Core.Public;
+using Server.Core.Model;
+
+namespace Server.Core
+{
+    public partial class VmBase
+    {
+        #region Data
+        //public void DoSmth()
+        //{
+        //    using (EntityServ _serv = new EntityServ(connectionString))
+        //    {
+        //        List<BUILD> items = _serv.Get_BUILD().ToList();
+        //        List<NSI_VILLAGE> items1 = _serv.Get_NSI_VILLAGE().ToList();
+        //    }
+        //}
+
+        public IEnumerable<VW_NSI_VILLAGE> VwNsiVillages
+        {
+            get
+            {
+                using (EntityServ _serv = new EntityServ(connectionString))
+                {
+                    //List<BUILD> itemsL = _serv.Get_BUILD().ToList();
+                    { }
+                    vwNsiVillages = _serv.Get_VW_NSI_VILLAGE().ToList();
+                }
+                return vwNsiVillages;
+            }
+        }
+        private IEnumerable<VW_NSI_VILLAGE> vwNsiVillages;
+
+        //public IEnumerable<VW_NSI_STREET> VwNsiStreets
+        //{
+        //    get
+        //    {
+        //        using (EntityServ _serv = new EntityServ(connectionString))
+        //        {
+        //            List<NSI_VILLAGE> itemsL = _serv.Get_NSI_VILLAGE().ToList();
+        //            { }
+        //            vwNsiStreets = _serv.Get_VW_NSI_STREET().ToList();
+        //        }
+        //        return vwNsiStreets;
+        //    }
+        //}
+        //private IEnumerable<VW_NSI_STREET> vwNsiStreets;
+        #endregion
+
+    }
+}
--- Core01/Server.Core/DataModel/DataGos/_Service.cs
+++ Core01/Server.Core/DataModel/DataGos/_Service.cs
@@ -14,12 +14,12 @@ namespace Server.Core.Model
     
     public partial class EntityServ : EntityService<EntityContext>
     {
-        public IQueryable<Partners>  Get_Partners() => Context.Partners;
-        public IQueryable<payerlive>  Get_payerlive() => Context.payerlive;
-        public IQueryable<rgn>  Get_rgn() => Context.rgn;
-        public IQueryable<street>  Get_street() => Context.street;
-        public IQueryable<type_street>  Get_type_street() => Context.type_street;
-        public IQueryable<type_village>  Get_type_village() => Context.type_village;
-        public IQueryable<village>  Get_village() => Context.village;
+        public IQueryable<Partners> Get_Partners() => Context.Partners;
+        public IQueryable<payerlive> Get_payerlive() => Context.payerlive;
+        public IQueryable<rgn> Get_rgn() => Context.rgn;
+        public IQueryable<street> Get_street() => Context.street;
+        public IQueryable<type_street> Get_type_street() => Context.type_street;
+        public IQueryable<type_village> Get_type_village() => Context.type_village;
+        public IQueryable<village> Get_village() => Context.village;
     }
 }
