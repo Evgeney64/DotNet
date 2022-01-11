e395fa9aab203dfa39440920096d69932083c355
Evgeny <Evgeny@technosb22.tsb.ru>
2022-01-11 16:08:24 +0500

    20211010-01

--- Extentions/MvcControls/Scripts/Webworker/webworker.js
+++
@@ -1,4 +0,0 @@
-﻿/// <reference path="JQuery/jquery.signalR-2.2.1.min.js" />
-
-self.importScripts("Scripts/JQuery/jquery.signalR-2.2.1.min.js");
-self.importScripts("signalr/hubs");
--- Extentions/MvcControls/Scripts/telephony/onclick.js
+++
@@ -1,9 +0,0 @@
-﻿/*
-$(document).ready(function () {
-    $("body").on("click", "a[href^='tel:']", function (e) {
-        e.preventDefault();
-        $.get("../api/Telephony/Call", { to: $(e.target).attr("href").substring(4) });
-        return false;
-    });
-});
-*/
